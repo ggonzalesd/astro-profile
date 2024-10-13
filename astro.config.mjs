4ab280f2861ade59cedbf2e036ca17ea9e56aaa0
Grober Gonzales <GonzalesDLCGrober@gmail.com>
2024-10-13 15:49:37 -0500

    Sitemap Implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/static';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   i18n: {
@@ -12,8 +14,8 @@ export default defineConfig({
       prefixDefaultLocale: false,
     },
   },
-
-  integrations: [tailwind()],
+  site: 'https://ggonzalesd.vercel.app',
+  integrations: [tailwind(), sitemap()],
   output: 'static',
   adapter: vercel(),
 });
